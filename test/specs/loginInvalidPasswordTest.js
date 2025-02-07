a96e7e56f075f1d1c863cba6946d99156369cce4
Polina <gurighan511.com>
2025-02-07 10:31:37 +0100

    Added allert in loginInvallidPasswordTest

--- test/specs/loginInvalidPasswordTest.js
+++ test/specs/loginInvalidPasswordTest.js
@@ -1,15 +1,18 @@
 const loginPage = require('../pageobjects/login.page');
 
 describe('Sauce Demo Login Test - Invalid Password', () => {
-    it('should show error for invalid password', async () => {
+    it('should fail to login with an invalid password and show error indicators', async () => {
         await loginPage.open();  
         
         await loginPage.login('standard_user', 'wrong_password');  
-
-        expect(await browser.getUrl()).not.toContain('inventory.html');
-
+        
         expect(await loginPage.errorMessage.isDisplayed()).toBeTruthy();
+        
+        expect(await loginPage.errorMessage.getText()).toContain(
+            'Epic sadface: Username and password do not match any user in this service'
+        );
 
+        expect(await loginPage.errorIconUsername.isDisplayed()).toBeTruthy();
         expect(await loginPage.errorIconPassword.isDisplayed()).toBeTruthy();
     });
 });
